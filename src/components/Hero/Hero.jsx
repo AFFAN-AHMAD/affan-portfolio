efdb21b400fed9b5205cc09946c3c395c8155f7d
affan <affan.ahmad@superprocure.com>
2025-07-09 21:20:29 +0530

    9 july: created Hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.svg";
+const Hero = () => {
+  return (
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1>Hi, I'm Affan Ahmad</h1>
+      <p>
+        I am a full stack web developer from Delhi, India with 2.5 years of
+        experience.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My Resume</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
